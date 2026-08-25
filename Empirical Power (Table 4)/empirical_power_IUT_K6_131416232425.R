78d97a66d5339cad423442f095684a4ceab30fa0
Arpan Singh <53208316+arpan52@users.noreply.github.com>
2026-08-25 13:46:13 +0100

    Update empirical_power_IUT_K6_131416232425.R

--- Empirical Power (Table 4)/empirical_power_IUT_K6_131416232425.R
+++ Empirical Power (Table 4)/empirical_power_IUT_K6_131416232425.R
@@ -1,110 +1,161 @@
 rm(list = ls())
-true.mean <- c(54.5,55.2,62.1,61.8,63.3,64.9)                        # Sample mean
-K<-length(true.mean)                                                # No. of comparison groups 
-psd<- 5.90                                                           # Pooled standard deviation
 
-n_m<- c(11,11,7,7,6,6)  # max-min design
-n_b= c(8,8,8,9,9,9)   # balance design
+true.mean <- c(84.2,94.4,88.3,90.3,90.1,92.5)#c(84.2,94.4,92.5,90.3,90.1,88.3) #c(97.8, 96.3, 94, 94.2, 93.7, 91.9)
+K <- length(true.mean)                                  # No. of comparison groups
+psd <- 9.19                                              # Pooled standard deviation
 
-m<-10000                                             #No. of Iterations
+n_m <- c(112,112,84,84,72,72) #c(103,103,77,77,66,66)                       # Max-min design
+n_b <-  c(102,102,84,84,59,59)                       # C-optimal design
+#n_b <-  c(83,78,79,82,90,80)                     # Balanced design
+#n_b<- c(100,100,79,79,67,67)
+m <- 100000                                      # No. of iterations
 
+
+# ---------------------------------------------------------
 # Initializations
+# ---------------------------------------------------------
 
-X.m_mean<-rep(0,K)
-X.b_mean<-rep(0,K)
+Y.m_mean <- rep(0, K)
+Y.b_mean <- rep(0, K)
 
-Y.m_mean<-rep(0,K)
-Y.b_mean<-rep(0,K)
-
-
-t_stat_m<-rep(0,m)
-t_stat_b<-rep(0,m)
+ctr_m <- 0
+ctr_b <- 0
 
 
-ctr_m<-0
-ctr_b<-0
+# ---------------------------------------------------------
+# Critical value
+# ---------------------------------------------------------
+# One-sided test at alpha = 0.05
+# No null simulation and no multiplicity correction
+
+alpha <- 0.05
+q <- qnorm(1 - alpha)
+
+print("Critical value")
+print(q)
 
 
-for (i in 1:m) { 
-  for (j in 1:K){
-    X.m <- rnorm(n_m[j],true.mean[1],psd)  # null data from max--min design
-    X.m_mean[j] <- mean(X.m)
-    
-    X.b <- rnorm(n_b[j],true.mean[1],psd)  # null data from balanced design
-    X.b_mean[j] <- mean(X.b)
+# ---------------------------------------------------------
+# Simulation under the alternative
+# ---------------------------------------------------------
+
+for (i in 1:m) {
+  
+  # -------------------------------------------------------
+  # Alternative data: Max-min design
+  # -------------------------------------------------------
+  
+  for (j in 1:K) {
+    Y.m <- rnorm(n_m[j], true.mean[j], psd)
+    Y.m_mean[j] <- mean(Y.m)
   }
   
-  Z_13_m = (X.m_mean[1]-X.m_mean[3])/(psd*sqrt((1/n_m[1])+(1/n_m[3])))
-  Z_14_m = (X.m_mean[1]-X.m_mean[4])/(psd*sqrt((1/n_m[1])+(1/n_m[4])))
-  Z_16_m = (X.m_mean[1]-X.m_mean[6])/(psd*sqrt((1/n_m[1])+(1/n_m[6])))
-  Z_23_m = (X.m_mean[2]-X.m_mean[3])/(psd*sqrt((1/n_m[2])+(1/n_m[3])))
-  Z_24_m = (X.m_mean[2]-X.m_mean[4])/(psd*sqrt((1/n_m[2])+(1/n_m[4])))
-  Z_25_m = (X.m_mean[2]-X.m_mean[5])/(psd*sqrt((1/n_m[2])+(1/n_m[5])))
   
-
-  t_stat_m[i]<- min(c(abs(Z_13_m),abs(Z_14_m),abs(Z_16_m),abs(Z_23_m),abs(Z_24_m),abs(Z_25_m)))                          #test statistics under the null for max-min design
+  # -------------------------------------------------------
+  # Test statistics for Max-min design
+  # Comparisons: (1,3), (1,4), (1,6),
+  #              (2,3), (2,4), (2,5)
+  # -------------------------------------------------------
   
-  Z_13_b = (X.b_mean[1]-X.b_mean[3])/(psd*sqrt((1/n_b[1])+(1/n_b[3])))
-  Z_14_b = (X.b_mean[1]-X.b_mean[4])/(psd*sqrt((1/n_b[1])+(1/n_b[4])))
-  Z_16_b = (X.b_mean[1]-X.b_mean[6])/(psd*sqrt((1/n_b[1])+(1/n_b[6])))
-  Z_23_b = (X.b_mean[2]-X.b_mean[3])/(psd*sqrt((1/n_b[2])+(1/n_b[3])))
-  Z_24_b = (X.b_mean[2]-X.b_mean[4])/(psd*sqrt((1/n_b[2])+(1/n_b[4])))
-  Z_25_b = (X.b_mean[2]-X.b_mean[5])/(psd*sqrt((1/n_b[2])+(1/n_b[5])))
-
+  Z_13_m <- (Y.m_mean[1] - Y.m_mean[3]) /
+    (psd * sqrt((1/n_m[1]) + (1/n_m[3])))
+  
+  Z_14_m <- (Y.m_mean[1] - Y.m_mean[4]) /
+    (psd * sqrt((1/n_m[1]) + (1/n_m[4])))
+  
+  Z_16_m <- (Y.m_mean[1] - Y.m_mean[6]) /
+    (psd * sqrt((1/n_m[1]) + (1/n_m[6])))
+  
+  Z_23_m <- (Y.m_mean[2] - Y.m_mean[3]) /
+    (psd * sqrt((1/n_m[2]) + (1/n_m[3])))
+  
+  Z_24_m <- (Y.m_mean[2] - Y.m_mean[4]) /
+    (psd * sqrt((1/n_m[2]) + (1/n_m[4])))
+  
+  Z_25_m <- (Y.m_mean[2] - Y.m_mean[5]) /
+    (psd * sqrt((1/n_m[2]) + (1/n_m[5])))
   
   
-  t_stat_b[i]<- min(c(abs(Z_13_b),abs(Z_14_b),abs(Z_16_b),abs(Z_23_b),abs(Z_24_b),abs(Z_25_b)))                          #test statistics under the null for balanced design
+  # Minimum absolute test statistic
+  t_stat_m <- min(c(
+    abs(Z_13_m),
+    abs(Z_14_m),
+    abs(Z_16_m),
+    abs(Z_23_m),
+    abs(Z_24_m),
+    abs(Z_25_m)
+  ))
   
-}
-
-q_m<- quantile(t_stat_b,probs = 0.95)                                  # Critical value for Max--min design
-q_b<- quantile(t_stat_b,probs = 0.95)                                 # Critical value for Max--min design
-
-
-for (i in 1:m) { 
-  for (j in 1:K){
-    Y.m <- rnorm(n_m[j],true.mean[j],psd)  # alternative data from max--min design
-    Y.m_mean[j] <- mean(Y.m)
-    
-    Y.b <- rnorm(n_b[j],true.mean[j],psd)  # alternative data from balanced design
+  
+  # Reject H0 if minimum absolute statistic exceeds q
+  if (t_stat_m > q) {
+    ctr_m <- ctr_m + 1
+  }
+  
+  
+  # -------------------------------------------------------
+  # Alternative data: C-optimal design
+  # -------------------------------------------------------
+  
+  for (j in 1:K) {
+    Y.b <- rnorm(n_b[j], true.mean[j], psd)
     Y.b_mean[j] <- mean(Y.b)
   }
-  Z_13_m.A = (Y.m_mean[1]-Y.m_mean[3])/(psd*sqrt((1/n_m[1])+(1/n_m[3])))
-  Z_14_m.A = (Y.m_mean[1]-Y.m_mean[4])/(psd*sqrt((1/n_m[1])+(1/n_m[4])))
-  Z_16_m.A = (Y.m_mean[1]-Y.m_mean[6])/(psd*sqrt((1/n_m[1])+(1/n_m[6])))
-  Z_23_m.A = (Y.m_mean[2]-Y.m_mean[3])/(psd*sqrt((1/n_m[2])+(1/n_m[3])))
-  Z_24_m.A = (Y.m_mean[2]-Y.m_mean[4])/(psd*sqrt((1/n_m[2])+(1/n_m[4])))
-  Z_25_m.A = (Y.m_mean[2]-Y.m_mean[5])/(psd*sqrt((1/n_m[2])+(1/n_m[5])))
-
   
   
-  t_stat_m.A<- min(c(abs(Z_13_m.A),abs(Z_14_m.A),abs(Z_16_m.A),abs(Z_23_m.A),abs(Z_24_m.A),abs(Z_25_m.A)))              #test statistics under the altenative for max-min design
+  # -------------------------------------------------------
+  # Test statistics for C-optimal design
+  # Comparisons: (1,3), (1,4), (1,6),
+  #              (2,3), (2,4), (2,5)
+  # -------------------------------------------------------
   
-  if(t_stat_m.A>q_m){
-    ctr_m <- ctr_m+1
-  }
-  Z_13_b.A = (Y.b_mean[1]-Y.b_mean[3])/(psd*sqrt((1/n_b[1])+(1/n_b[3])))
-  Z_14_b.A = (Y.b_mean[1]-Y.b_mean[4])/(psd*sqrt((1/n_b[1])+(1/n_b[4])))
-  Z_16_b.A = (Y.b_mean[1]-Y.b_mean[6])/(psd*sqrt((1/n_b[1])+(1/n_b[6])))
-  Z_23_b.A = (Y.b_mean[2]-Y.b_mean[3])/(psd*sqrt((1/n_b[2])+(1/n_b[3])))
-  Z_24_b.A = (Y.b_mean[2]-Y.b_mean[4])/(psd*sqrt((1/n_b[2])+(1/n_b[4])))
-  Z_25_b.A = (Y.b_mean[2]-Y.b_mean[5])/(psd*sqrt((1/n_b[2])+(1/n_b[5])))
-
+  Z_13_b <- (Y.b_mean[1] - Y.b_mean[3]) /
+    (psd * sqrt((1/n_b[1]) + (1/n_b[3])))
+  
+  Z_14_b <- (Y.b_mean[1] - Y.b_mean[4]) /
+    (psd * sqrt((1/n_b[1]) + (1/n_b[4])))
+  
+  Z_16_b <- (Y.b_mean[1] - Y.b_mean[6]) /
+    (psd * sqrt((1/n_b[1]) + (1/n_b[6])))
+  
+  Z_23_b <- (Y.b_mean[2] - Y.b_mean[3]) /
+    (psd * sqrt((1/n_b[2]) + (1/n_b[3])))
+  
+  Z_24_b <- (Y.b_mean[2] - Y.b_mean[4]) /
+    (psd * sqrt((1/n_b[2]) + (1/n_b[4])))
+  
+  Z_25_b <- (Y.b_mean[2] - Y.b_mean[5]) /
+    (psd * sqrt((1/n_b[2]) + (1/n_b[5])))
   
   
-  t_stat_b.A<- min(c(abs(Z_13_b.A),abs(Z_14_b.A),abs(Z_16_b.A),abs(Z_23_b.A),abs(Z_24_b.A),abs(Z_25_b.A)))                    #test statistics under the altenative for balanced design
+  # Minimum absolute test statistic
+  t_stat_b <- min(c(
+    abs(Z_13_b),
+    abs(Z_14_b),
+    abs(Z_16_b),
+    abs(Z_23_b),
+    abs(Z_24_b),
+    abs(Z_25_b)
+  ))
   
-  if (t_stat_b.A>q_b){
-    ctr_b <- ctr_b+1
+  
+  # Reject H0 if minimum absolute statistic exceeds q
+  if (t_stat_b > q) {
+    ctr_b <- ctr_b + 1
   }
   
 }
 
 
-print('CV max-min, CV balanced')
-print(c(q_m,q_b))
+# ---------------------------------------------------------
+# Results
+# ---------------------------------------------------------
 
-print('power max-min, power balanced')
-print(c(ctr_m/m,ctr_b/m))
+print("Critical value")
+print(q)
 
-
+print("Power: max-min design, C-optimal design")
+print(c(
+  ctr_m / m,
+  ctr_b / m
+))
